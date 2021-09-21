3adb1a8ec86d5f62b7cc42c7f49b0a3da79be61c
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2021-09-21 10:40:28 +0530

    feat: get user function

---
+++ Inovact_getUser/index.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+
+exports.handler = (events, context, callback) => {
+  const id = events.id
+
+  if (typeof id == 'string') {
+    const query = `
+      query getUser($id: Int_comparison_exp) {
+        user(where: { id: $id }) {
+          id,
+          user_name,
+          bio,
+          avatar,
+          phone_number,
+          email_id,
+          designation,
+          organization,
+          organizational_role,
+          summary,
+          university,
+          graduation_year,
+          journey_start_date,
+          years_of_personal_experience
+        }
+      }
+    `;
+
+    const variables = {
+      id: {
+        _eq: id
+      }
+    };
+
+    axios
+      .post(
+          process.env.HASURA_API,
+          { query, variables },
+          {
+              headers: {
+                  "content-type": "application/json",
+                  "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET,
+              },
+          }
+      )
+      .then((res) => {
+          callback(null, res.data);
+      })
+      .catch((err) => {
+          callback(err);
+      });
+  } else {
+    callback({
+      statusCode: 400,
+      message: "Invalid or missing id"
+    });
+  }
+}
