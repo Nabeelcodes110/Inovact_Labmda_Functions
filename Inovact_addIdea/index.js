6739b1594b57e0b43b15c7398638bdb70941b54d
Abhishek raut <56253046+abhishek23012000@users.noreply.github.com>
2021-09-21 20:16:39 +0530

    Add files via upload

---
+++ Inovact_addIdea/index.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+exports.handler = (event, context, callback) => {
+  const query = `mutation add_idea($caption: String!,$description: String!,$title:String!,$user_id:Int,$url:String!) {
+  insert_idea(objects: [{
+    caption: $caption
+    description: $description
+    user_id: $user_id
+		title:$title
+		url:$url
+  }]) {
+    returning {
+      id
+      created_at
+      updated_at
+    }
+  }
+}`;
+
+  let variables = {
+    caption: event.caption,
+    description: event.description,
+    title: event.title,
+    user_id: event.user_id,
+    url: event.url,
+  };
+  axios
+    .post(
+      process.env.HASURA_API,
+
+      { query, variables },
+      {
+        headers: {
+          "content-type": "application/json",
+          "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET,
+        },
+      }
+    )
+    .then((res) => {
+      console.log(res.data);
+      callback(null, res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      callback(err);
+    });
+  // callback(null, event);
+};
