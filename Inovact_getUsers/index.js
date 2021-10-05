01ed3dabbd450ca97880d8e7eb18a454183bd490
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2021-10-05 09:54:40 +0530

    upd: added new fields

--- Inovact_getUsers/index.js
+++ Inovact_getUsers/index.js
@@ -13,11 +13,18 @@ exports.handler = (events, context, callback) => {
         designation,
         organization,
         organizational_role,
-        summary,
         university,
         graduation_year,
         journey_start_date,
-        years_of_personal_experience
+        years_of_professional_experience,
+        created_at,
+        updated_at,
+        first_name,
+        last_name,
+        role,
+        cognito_sub,
+        admin,
+        profile_complete
       }
     }
   `;
