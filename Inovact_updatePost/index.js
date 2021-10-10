052678d631be712f602ac549173dadefe31f68c3
Abhishek raut <56253046+abhishek23012000@users.noreply.github.com>
2021-10-10 23:56:43 +0530

    added new folder structure

--- Inovact_updatePost/index.js
+++ Inovact_updatePost/index.js
@@ -1,43 +1,25 @@
-const axios = require('axios');
+const axios = require("axios");
+const { query: Hasura } = require("./utils/hasura");
+const { updatePost_query } = require("./queries/queries");
 
-exports.handler = (events, context, callback) => {
-  const query = `
-    mutation updateProject($id: Int_comparison_exp, $changes: project_set_input) {
-      update_project(where: { id: $id }, _set: $changes) {
-        returning {
-          id
-        }
-      }
-    }
-  `;
-
-  let variables = {
+exports.handler = async (events, context, callback) => {
+  let variables = await {
     id: {
       _eq: events.id,
     },
     changes: {},
   };
 
-  if (events.caption) variables['changes']['caption'] = events.caption;
+  if (events.caption) variables["changes"]["caption"] = events.caption;
   if (events.description)
-    variables['changes']['description'] = events.description;
-  if (events.title) variables['changes']['title'] = events.title;
+    variables["changes"]["description"] = events.description;
+  if (events.title) variables["changes"]["title"] = events.title;
 
-  axios
-    .post(
-      process.env.HASURA_API,
-      { query, variables },
-      {
-        headers: {
-          'content-type': 'application/json',
-          'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
-        },
-      }
-    )
-    .then(res => {
-      callback(null, res.data);
-    })
-    .catch(err => {
-      callback(err);
-    });
+  const response = await Hasura(updatePost_query, variables);
+
+  if (response.success) {
+    callback(null, response.result);
+  } else {
+    callback(null, response.errors);
+  }
 };
