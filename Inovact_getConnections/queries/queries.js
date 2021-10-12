06b935b56b49fd21e8abf3f0d24a07d0bf7e4539
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2021-10-12 10:55:25 +0530

    bugfix: fixed graphql query

--- Inovact_getConnections/queries/queries.js
+++ Inovact_getConnections/queries/queries.js
@@ -6,12 +6,11 @@ const getUserId = `query getUser($cognito_sub: String_comparison_exp) {
 `;
 
 const getUserConnections = `query getMyConnections($user_id: Int_comparison_exp) {
-  connections(where: {_or: {user1: $user_id, user2: $user_id}}) {
+  connections(where: { _or: [{ user1: $user_id }, { user2: $user_id}]}) {
     user1
     user2
     created_at
     formed_at
-    initiated_by
     status
   }
 }`;
