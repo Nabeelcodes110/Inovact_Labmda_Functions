60b5fa232c6f455f6b7e8a144eb1999421651bb2
Abhishek raut <56253046+abhishek23012000@users.noreply.github.com>
2021-10-15 01:35:13 +0530

    added logic to fetch a single Idea

--- Inovact_getIdea/index.js
+++ Inovact_getIdea/index.js
@@ -1,55 +1,31 @@
 const axios = require('axios');
-exports.handler = (event, context, callback) => {
-  const query = `query getIdea {
-    idea {
-      id
-      title
-      description
-      user_id
-      idea_tags {
-        hashtag {
-          name
-        }
-      }
-      idea_likes {
-        user_id
-      }
-      idea_comments {
-        id
-        created_at
-        text
-        updated_at
-        user_id
-      }
-      idea_documents {
-        id
-        name
-        url
-        uploaded_at
-      }
-      created_at
-      updated_at
-    }
-  }
-  `;
-  axios
-    .post(
-      process.env.HASURA_API,
+const { query: Hasura } = require('./utils/hasura');
+const { getIdea,getIdeas } = require('./queries/queries');
 
-      { query, variables: {} },
-      {
-        headers: {
-          'content-type': 'application/json',
-          'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
-        },
-      }
-    )
-    .then(res => {
-      console.log(res.data);
-      callback(null, res.data);
-    })
-    .catch(err => {
-      console.log(err);
-      callback(err);
-    });
+exports.handler = async (events, context, callback) => {
+  const id = await events.id;
+
+  if (id) {
+    const variables = await {
+      id
+    }
+
+    const response1 = await Hasura(getIdea, variables);
+
+    if (!response1.success) return callback(null, response1.errors);
+
+    callback(null, response1.result);
+    
+    
+  } else {
+     const response = await Hasura(getIdeas);
+
+  if (response.success) {
+    callback(null, response.result);
+  } else {
+    callback(null, response.errors);
+  }
+    
+  }
+  
 };
---
+++ Inovact_getIdea/queries/queries.js
@@ -0,0 +1,72 @@
+  const getIdeas = `query getIdeas {
+    idea {
+      id
+      title
+      description
+      user_id
+      idea_tags {
+        hashtag {
+          name
+        }
+      }
+      idea_likes {
+        user_id
+      }
+      idea_comments {
+        id
+        created_at
+        text
+        updated_at
+        user_id
+      }
+      idea_documents {
+        id
+        name
+        url
+        uploaded_at
+      }
+      created_at
+      updated_at
+    }
+  }
+  `;
+  
+
+   const getIdea = `query getIdea($id: Int) {
+    idea (where: { id: { _eq: $id }}) {
+      id
+      title
+      description
+      user_id
+      idea_tags {
+        hashtag {
+          name
+        }
+      }
+      idea_likes {
+        user_id
+      }
+      idea_comments {
+        id
+        created_at
+        text
+        updated_at
+        user_id
+      }
+      idea_documents {
+        id
+        name
+        url
+        uploaded_at
+      }
+      created_at
+      updated_at
+    }
+  }
+  `;
+  
+  module.exports = {
+ getIdea,
+ getIdeas
+
+};
