3ddffc702b2dd843bad9029ba5e44733affba180
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2021-10-15 15:04:54 +0530

    upd: removed unnecessary awaits

--- Inovact_getIdea/index.js
+++ Inovact_getIdea/index.js
@@ -1,31 +1,27 @@
 const axios = require('axios');
 const { query: Hasura } = require('./utils/hasura');
-const { getIdea,getIdeas } = require('./queries/queries');
+const { getIdea, getIdeas } = require('./queries/queries');
 
 exports.handler = async (events, context, callback) => {
   const id = await events.id;
 
   if (id) {
-    const variables = await {
-      id
-    }
+    const variables = {
+      id,
+    };
 
     const response1 = await Hasura(getIdea, variables);
 
     if (!response1.success) return callback(null, response1.errors);
 
     callback(null, response1.result);
-    
-    
   } else {
-     const response = await Hasura(getIdeas);
+    const response = await Hasura(getIdeas);
 
-  if (response.success) {
-    callback(null, response.result);
-  } else {
-    callback(null, response.errors);
+    if (response.success) {
+      callback(null, response.result);
+    } else {
+      callback(null, response.errors);
+    }
   }
-    
-  }
-  
 };
