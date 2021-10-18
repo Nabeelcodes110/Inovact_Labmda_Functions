f3e48e3553fd2f70c7f7b0ce7d4ce2f884c6cdce
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2021-10-18 13:50:17 +0530

    chore: added missing getTeam query

--- Inovact_getTeams/queries/queries.js
+++ Inovact_getTeams/queries/queries.js
@@ -52,6 +52,59 @@ const getUserTeams = `query getMyTeams($user_id: Int) {
 }
 `;
 
+const getTeam = `query getTeam($team_id: Int) {
+  team(where: {id: { _eq: $team_id }}) {
+    name
+    avatar
+    looking_for_members
+    looking_for_mentors
+    team_role_requirements {
+      role {
+        id
+        name
+      }
+      team_skill_requirements {
+        skill {
+          id
+          name
+        }
+      }
+    }
+    team_invitations {
+      invited_at
+      user {
+        id
+        first_name
+        last_name
+      }
+    }
+    team_requests {
+      user_id
+      requested_on
+    }
+    team_members {
+      joined_date
+      admin
+      user {
+        avatar
+        first_name
+        last_name
+        role
+      }
+    }
+    team_documents {
+      id
+      name
+      mime_type
+      uploaded_at
+    }
+    projects {
+      title
+      status
+    }
+  }
+}`;
+
 const getUserId = `query getUser($cognito_sub: String_comparison_exp) {
   user(where: { cognito_sub: $cognito_sub }) {
     id
@@ -62,4 +115,5 @@ const getUserId = `query getUser($cognito_sub: String_comparison_exp) {
 module.exports = {
   getUserTeams,
   getUserId,
+  getTeam,
 };
