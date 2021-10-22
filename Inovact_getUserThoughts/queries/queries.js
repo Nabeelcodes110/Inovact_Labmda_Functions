e3da75d0e411f5bff809c1a3a57d3ce06d62307a
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2021-10-22 23:37:37 +0530

    upd: updated returned data

--- Inovact_getUserThoughts/queries/queries.js
+++ Inovact_getUserThoughts/queries/queries.js
@@ -1,20 +1,56 @@
 const getUserThoughts = `query getUserThoughts($user_id: Int) {
   thoughts(where: {user_id: {_eq: $user_id}}) {
-    user_id
-    thought
     id
+    thought
+    user_id
+    
+    thought_likes {
+      user_id
+    }
+    thought_comments {
+      id
+      created_at
+      updated_at
+      user_id
+    }
+  
     created_at
     updated_at
+    user {
+      id
+      avatar
+      first_name
+      last_name
+      role
+    }
   }
 }`;
 
 const getUserThoughtsWithCognitoSub = `query getUserThoughts($cognito_sub: String) {
   thoughts(where: { user: { cognito_sub: { _eq: $cognito_sub }}}) {
-    user_id
-    thought
     id
+    thought
+    user_id
+    
+    thought_likes {
+      user_id
+    }
+    thought_comments {
+      id
+      created_at
+      updated_at
+      user_id
+    }
+  
     created_at
     updated_at
+    user {
+      id
+      avatar
+      first_name
+      last_name
+      role
+    }
   }
 }`;
 
