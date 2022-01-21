845ee04149a86dffa9d3d2595076b45b05abbd2e
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2022-01-21 13:00:01 +0530

    chore: now sending profile_complete parameter

--- Inovact_mobile_signIn/index.js
+++ Inovact_mobile_signIn/index.js
@@ -1,4 +1,6 @@
 const { signIn, getCognitoUser } = require('./utils/cognito');
+const { query: Hasura } = require('./utils/hasura');
+const { getProfileComplete } = require('./queries/queries');
 
 exports.handler = async (events, context, callback) => {
   // Check if already exists but email not verified
@@ -23,9 +25,23 @@ exports.handler = async (events, context, callback) => {
   }
 
   // @TODO Sanitize input
-  const result = await signIn(events.email, events.password).catch(err => {
+  let result = await signIn(events.email, events.password).catch(err => {
     return err;
   });
+
+  if (!result.success) return callback(null, result);
+
+  const response = await Hasura(getProfileComplete, { email: events.email });
+
+  if (!response.success)
+    return callback(null, {
+      success: false,
+      errorCode: 'InternalServerError',
+      errorMessage: 'Failed to fetch profile complete of user',
+    });
+
+  result.data['profile_complete'] =
+    response.result.data.user[0].profile_complete;
 
   callback(null, result);
 };
