b00c046e3954d4c2bb1d15811581c8ba4bfb118f
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2022-02-18 20:16:09 +0530

    upd: return format

--- Inovact_addComment/index.js
+++ Inovact_addComment/index.js
@@ -33,16 +33,42 @@ exports.handler = async (events, context, callback) => {
       article_id
     );
   } else {
-    return callback('invalid article type provided');
+    return callback(null, {
+      success: false,
+      errorCode: 'INVALID_ARTICLE_TYPE',
+      errorMessage: 'Invalid article type provided',
+      data: null,
+    });
   }
 
-  if (!response.success) return callback(null, response.errors);
+  if (!response.success)
+    return callback(null, {
+      success: false,
+      errorCode: 'InternalServerError',
+      errorMessage: JSON.stringify(response.errors),
+      data: null,
+    });
 
   if (article_type == 'post') {
-    callback(null, response.result.data.insert_project_comment[0]);
+    callback(null, {
+      success: true,
+      errorCode: '',
+      errorMessage: '',
+      data: response.result.data.insert_project_comment.returning[0],
+    });
   } else if (article_type == 'idea') {
-    callback(null, response.result.data.insert_idea_comment[0]);
+    callback(null, {
+      success: true,
+      errorCode: '',
+      errorMessage: '',
+      data: response.result.data.insert_idea_comment.returning[0],
+    });
   } else {
-    callback(null, response.result.data.insert_thought_comments[0]);
+    callback(null, {
+      success: true,
+      errorCode: '',
+      errorMessage: '',
+      data: response.result.data.insert_thought_comments.returning[0],
+    });
   }
 };
