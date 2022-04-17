939940384e013e1eaea832b504f705d8b5b53cad
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2022-04-17 14:43:39 +0530

    chore: fixed whatever tf was happening with this

--- Inovact_addTeamDocument/index.js
+++ Inovact_addTeamDocument/index.js
@@ -1,63 +1,53 @@
 const { query: Hasura } = require('./utils/hasura');
-const { add_TeamDocument} = require('./queries/mutations');
-const { getUserId, getTeamDocument,getTeamUserId } = require('./queries/queries');
+const { add_TeamDocument } = require('./queries/mutations');
+const { checkIfAdmin } = require('./queries/queries');
 
 exports.handler = async (events, context, callback) => {
-  // Find user id
-  const cognito_sub = events.cognito_sub;
-  const response1 = await Hasura(getUserId, {
-    cognito_sub: { _eq: cognito_sub },
+  // Check if current user is team admin
+  const response1 = await Hasura(checkIfAdmin, {
+    cognito_sub: events.cognito_sub,
+    team_id: events.team_id,
   });
 
-  // If failed to find user return error
-  if (!response1.success) callback(null, response1.errors);
-  if (response1.result.data.user.length == 0) callback('User not found');
-  
-  //getTeamUserId
-  const variable = {
-    team_id: events.team_id
-    
-    
-  };
-  
-  //getTeamuserId
-  const response = await Hasura(getTeamUserId, variable);
- if (!response.success) callback(null, response.errors);
- 
-  if(response.result.data.team_members[0].user_id==response1.result.data.user[0].id){
-    
-     const teamDocumentData = {
-    team_id: events.team_id,
-    mime_type:events.mime_type,
-    name:events.name,
-    url:events.url
-    
-  };
-
-  const response2 = await Hasura(add_TeamDocument, teamDocumentData);
-
-  // If failed to insert thought return error
-  if (!response2.success) return callback(null, response2.errors);
-
-  
-
-   
-  // Fetch the thought in final stage
-  const variables = {
-    id: response2.result.data.insert_team_documents.returning[0].id,
-  };
-
-  const response5 = await Hasura(getTeamDocument, variables);
-
-  if (!response5.success) callback(null, response5.errors);
-
-  callback(null, response5.result);
-    
-  }else{
-    callback(null,{
-      message: "Invalid or id not found",
-    });
+  if (!response1.success) {
+    return {
+      success: false,
+      errorCode: 'InternalServerError',
+      errorMessage: JSON.stringify(response1.errors),
+    };
   }
 
- 
+  if (
+    response1.result.data.team_members.length == 0 ||
+    !response1.result.data.team_members[0].admin
+  )
+    return callback(null, {
+      success: false,
+      errorCode: 'Forbidden',
+      errorMessage: 'You are not an admin of this team',
+      data: null,
+    });
+
+  // Upload the document info to Hasura
+  const response2 = await Hasura(add_TeamDocument, {
+    team_id: events.team_id,
+    name: events.name,
+    url: events.url,
+    mime_type: events.mime_type,
+  });
+
+  if (!response2.success) {
+    return {
+      success: false,
+      errorCode: 'InternalServerError',
+      errorMessage: JSON.stringify(response2.errors),
+      data: null,
+    };
+  }
+
+  callback(null, {
+    success: true,
+    errorCode: null,
+    errorMessage: null,
+  });
 };
--- Inovact_addTeamDocument/queries/queries.js
+++ Inovact_addTeamDocument/queries/queries.js
@@ -1,86 +1,9 @@
-// Requires Cognito_sub
-const getUserId = `query getUser($cognito_sub: String_comparison_exp) {
-  user(where: { cognito_sub: $cognito_sub }) {
-  id
- 
+const checkIfAdmin = `query checkIfAdmin($cognito_sub: String, $team_id: Int) {
+  team_members(where: {user: {cognito_sub: {_eq: $cognito_sub}}, team: {id: {_eq: $team_id}}}) {
+    admin
   }
-}
-`;
-
-const getTeamDocument = `query getTeamDocument($id: Int) {
-  team_documents(where: {id: {_eq: $id}}) {
-     id
-      mime_type
-      url
-      name
-      team{
-           id
-    name
-    avatar
-    looking_for_members
-    looking_for_mentors
-    team_role_requirements {
-      role {
-        id
-        name
-      }
-      team_skill_requirements {
-        skill {
-          id
-          name
-        }
-      }
-    }
-    team_invitations {
-      invited_at
-      user {
-        id
-        first_name
-        last_name
-      }
-    }
-    team_requests {
-      user_id
-      requested_on
-    }
-    team_members {
-      joined_date
-      admin
-      user {
-        id
-        avatar
-        first_name
-        last_name
-        role
-      }
-    }
-    team_documents {
-      id
-      name
-      mime_type
-      uploaded_at
-    }
-    projects {
-      title
-      status
-    }
-      }
-    
-  
-  }
-}
-`;
-
-const getTeamUserId = `query getTeamUserId($team_id: Int) {
-  team_members(where: {team_id: {_eq: $team_id}})  {
-  user_id
- 
-  }
-}
-`;
+}`;
 
 module.exports = {
-  getUserId,
- getTeamDocument,
- getTeamUserId
+  checkIfAdmin,
 };
