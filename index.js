b94a7695588267b4ca1712835cacfd308293200e
Akashchandra62 <akashchandra8544@gmail.com>
2023-03-20 14:23:48 +0530

    setup index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const env = reuqire('dotenv');
+//User Input -- > Function Name to call
+const prompt = require("prompt-sync")();    
+const functionName = prompt('Enter the function Name: ');
+
+//Getting the reder function from the user's input
+const render = require(`./Inovact_${functionName}/index.js`);
+
+//Environment set up for Evironment variables
+const env = require('dotenv');
 env.config();
+
+//Calling the render function with proper parametes and printing the response
+const response = render.handler({
+    prefix: "a"
+  }, null, (a, b) => {
+    console.log(b);
+  })
