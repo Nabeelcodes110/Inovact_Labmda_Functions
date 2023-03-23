d44317e6f770dd8ab4252198a80c7eb7ccc0afff
Akashchandra62 <akashchandra8544@gmail.com>
2023-03-23 21:56:50 +0530

    user input for render function

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const functionName = prompt('Enter the function Name: ');
 //Getting the reder function from the user's input
 const render = require(`./Inovact_${functionName}/index.js`);
 
+//Input required for function
+let arguments = JSON.parse(prompt('Enter the Arguments: '));
+
 //Environment set up for Evironment variables
 const env = require('dotenv');
 env.config();
 
 //Calling the render function with proper parametes and printing the response
-const response = render.handler({
-    prefix: "a"
-  }, null, (a, b) => {
+const response = render.handler(arguments, null, (a, b) => {
     console.log(b);
   })
