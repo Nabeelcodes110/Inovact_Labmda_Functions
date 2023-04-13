5e96bac5ed0f6beb73f230db598feca41baf59ce
Nabeelcodes110 <parvenabeel@gmail.com>
2023-04-13 20:15:01 +0530

    deleteUser route added

--- Inovact_deleteUser/queries/queries.js
+++
@@ -1,9 +0,0 @@
-const deleteUser = `mutation deleteUser($cognito_sub: String_comparison_exp) {
-    delete_user_by_pk(where: { cognito_sub: $cognito_sub }){
-        id
-    }
-}`;
-
-module.exports = {
-  deleteUser,
-};
