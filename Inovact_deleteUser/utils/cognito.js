9fab24b7705787aa1acd41b651153a0a0c8e5b9d
Nabeelcodes110 <parvenabeel@gmail.com>
2023-04-27 22:04:09 +0530

    Added function for deleting user from firebase

--- Inovact_deleteUser/utils/cognito.js
+++ Inovact_deleteUser/utils/cognito.js
@@ -1,32 +1,14 @@
-const {
-  CognitoUserPool,
-  AuthenticationDetails,
-  CognitoUser,
-} = require('amazon-cognito-identity-js');
-
-const AWS = require('aws-sdk');
-AWS.config.region = process.env.REGION;
-
-const identity = new AWS.CognitoIdentityServiceProvider();
-
-const poolData = {
-  UserPoolId: process.env.USER_POOL_ID, // Your user pool id here
-  ClientId: process.env.CLIENT_ID, // Your client id here
-};
-
-const userPool = new CognitoUserPool(poolData);
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 const deleteUser = async cognito_sub => {
-  await identity
-    .adminDeleteUser({
-      UserPoolId: poolData.UserPoolId,
-      UserID: cognito_sub,
-    })
-    .promise()
+  admin
+    .auth()
+    .deleteUser(cognito_sub)
     .then(() => {
       return true;
     })
-    .catch(err => {
+    .catch(error => {
       return false;
     });
 };
